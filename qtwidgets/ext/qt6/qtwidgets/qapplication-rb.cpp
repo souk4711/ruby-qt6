0ec2587056445036ceca5234151eae8664841024
John Doe <johndoe@example.com>
2025-08-19 08:09:48 +0800

    fix: Fix QtWidgets::QApplication#initialize: wrong argument type Array (expected Buffer ...)

--- qtwidgets/ext/qt6/qtwidgets/qapplication-rb.cpp
+++ qtwidgets/ext/qt6/qtwidgets/qapplication-rb.cpp
@@ -4,9 +4,21 @@ using namespace Rice;
 
 Rice::Class rb_cQApplication;
 
+QApplication *_newQApplication(std::vector<std::string> old_argv)
+{
+    std::vector<char *> argv;
+    for (int i = 0; i < old_argv.size(); ++i)
+    {
+        argv.push_back(const_cast<char *>(old_argv[i].c_str()));
+    }
+    int argc = argv.size();
+    return new QApplication(argc, argv.data());
+}
+
 void Init_QApplication(Rice::Module rb_mQt6QtWidgets)
 {
+    rb_mQt6QtWidgets.define_singleton_function("_new_qapplication", &_newQApplication, Arg("argv"));
+
     rb_cQApplication = define_class_under<QApplication, QGuiApplication>(rb_mQt6QtWidgets, "QApplication")
-                           .define_constructor(Constructor<QApplication, int &, char **>(), Arg("argc"), Arg("argv"))
                            .define_singleton_function("exec", &QApplication::exec);
 }
--- qtwidgets/lib/qt6/qtwidgets/qapplication.rb
+++ qtwidgets/lib/qt6/qtwidgets/qapplication.rb
@@ -3,6 +3,10 @@
 module Qt6
   module QtWidgets
     class QApplication
+      def self.new(argv = [])
+        ::Qt6::QtWidgets._new_qapplication(argv)
+      end
+
       def exec
         self.class.exec
       end
