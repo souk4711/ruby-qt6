0cf9c10e8b1df63b8e8f6a757d272f06fb7a5c64
John Doe <johndoe@example.com>
2025-08-21 00:35:01 +0800

    chore: Add qt6-declarative headers

---
+++ bindgen/qt6include/QtQml/6.9.1/QtQml/private/qml_compile_hash_p.h
@@ -0,0 +1,15 @@
+// Generated file, DO NOT EDIT
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#define QML_COMPILE_HASH "72316399fb5f744472e38a9e1449399e6dd9c880"
+#define QML_COMPILE_HASH_LENGTH 40
