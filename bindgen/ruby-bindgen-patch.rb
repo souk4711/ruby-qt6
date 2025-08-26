8db9801edf42fb13032b9eb0dc1b0c3fe2240bc2
John Doe <johndoe@example.com>
2025-08-27 01:34:10 +0800

    chore: Update bindgen task

--- bindgen/ruby-bindgen-patch.rb
+++ bindgen/ruby-bindgen-patch.rb
@@ -2,14 +2,12 @@ class RubyBindgen::Namer
   alias_method :_ruby, :ruby
   def ruby(cursor)
     if cursor.kind == :cursor_cxx_method
-      if cursor.spelling.match(/^[iI]s[A-Z_]/) && cursor.type.result_type.spelling == "bool"
-        "#{cursor.spelling.underscore.sub(/^is_/, "")}?"
-      elsif cursor.spelling.match(/^[sS]et[A-Z_]/) && cursor.num_arguments == 1
-        "#{cursor.spelling.underscore.sub(/^set_/, "")}="
-      elsif cursor.spelling.match(/^[gG]et[A-Z_]/) && cursor.num_arguments == 0
-        cursor.spelling.underscore.sub(/^get_/, "").to_s
-      else
+      if cursor.spelling.match(/^operator/)
         _ruby(cursor)
+      elsif cursor.spelling.match(/^[iI]s[A-Z_]/) && cursor.type.result_type.spelling == "bool"
+        "_#{cursor.spelling.underscore.sub(/^is_/, "")}?"
+      else
+        "_#{_ruby(cursor)}"
       end
     else
       _ruby(cursor)
