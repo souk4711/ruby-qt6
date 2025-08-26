3a63990be874d355c7085a0f9f61331f4ddf45a1
John Doe <johndoe@example.com>
2025-08-26 16:22:46 +0800

    docs: Update QtCore

--- qtcore/lib/qt6/qtcore/qanystringview.rb
+++ qtcore/lib/qt6/qtcore/qanystringview.rb
@@ -4,16 +4,22 @@ module RubyQt6
   module QtCore
     # @see https://doc.qt.io/qt-6/qanystringview.html
     class QAnyStringView
+      class << self
+        # @!visibility private
+        def new(str = "")
+          return str if str.is_a?(QAnyStringView)
+          allocate.tap { |o| o.__send__(:initialize, QString.new(str)) }
+        end
+      end
+
       # @!method initialize(str = "")
       #   @param str [QString]
       #   @return [QAnyStringView]
       #
       #   Constructs a string view on str.
-      #
+
       # @!visibility private
-      def self.new(str = "")
-        return str if str.is_a?(QAnyStringView)
-        allocate.tap { |o| o.__send__(:initialize, QString.new(str)) }
+      def _yardoc
       end
     end
   end
--- qtcore/lib/qt6/qtcore/qstring.rb
+++ qtcore/lib/qt6/qtcore/qstring.rb
@@ -4,16 +4,22 @@ module RubyQt6
   module QtCore
     # @see https://doc.qt.io/qt-6/qstring.html
     class QString
+      class << self
+        # @!visibility private
+        def new(str = "")
+          return str if str.is_a?(QString)
+          allocate.tap { |o| o.__send__(:initialize, str) }
+        end
+      end
+
       # @!method initialize(str = "")
       #   @param str [String]
       #   @return [QString]
       #
       #   Constructs a string on str.
-      #
+
       # @!visibility private
-      def self.new(str = "")
-        return str if str.is_a?(QString)
-        allocate.tap { |o| o.__send__(:initialize, str) }
+      def _yardoc
       end
     end
   end
