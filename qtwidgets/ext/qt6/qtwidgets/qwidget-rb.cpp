267cc0dc52074442ec573ad594fd3e7b2bd1ca75
John Doe <johndoe@example.com>
2025-08-27 01:30:07 +0800

    docs: Update QtWidgets

--- qtwidgets/ext/qt6/qtwidgets/qwidget-rb.cpp
+++ qtwidgets/ext/qt6/qtwidgets/qwidget-rb.cpp
@@ -10,15 +10,14 @@ void Init_qwidget(Rice::Module rb_mQt6QtWidgets)
     rb_cQWidget =
         // RubyQt6::QtWidgets::QWidget
         define_class_under<QWidget, QObject>(rb_mQt6QtWidgets, "QWidget")
-            .define_method("show", &QWidget::show)
-            .define_method("show_full_screen", &QWidget::showFullScreen)
-            .define_method("show_maximized", &QWidget::showMaximized)
-            .define_method("show_minimized", &QWidget::showMinimized)
-            .define_method("show_normal", &QWidget::showNormal)
-            .define_method("hide", &QWidget::hide)
-            .define_method("size", &QWidget::size)
-            .define_method<void (QWidget::*)(int, int)>("resize", &QWidget::resize, Arg("width"), Arg("height"))
-            .define_method<void (QWidget::*)(const QSize &)>("resize", &QWidget::resize, Arg("size"))
-            .define_method("font", &QWidget::font)
-            .define_method("font=", &QWidget::setFont, Arg("font"));
+            .define_method("_show", &QWidget::show)
+            .define_method("_show_full_screen", &QWidget::showFullScreen)
+            .define_method("_show_maximized", &QWidget::showMaximized)
+            .define_method("_show_minimized", &QWidget::showMinimized)
+            .define_method("_show_normal", &QWidget::showNormal)
+            .define_method("_hide", &QWidget::hide)
+            .define_method("_size", &QWidget::size)
+            .define_method<void (QWidget::*)(const QSize &)>("_resize", &QWidget::resize, Arg("size"))
+            .define_method("_font", &QWidget::font)
+            .define_method("_set_font", &QWidget::setFont, Arg("font"));
 }
--- qtwidgets/lib/qt6/qtwidgets/qwidget.rb
+++ qtwidgets/lib/qt6/qtwidgets/qwidget.rb
@@ -4,126 +4,138 @@ module RubyQt6
   module QtWidgets
     # @see https://doc.qt.io/qt-6/qwidget.html
     class QWidget < RubyQt6::QtCore::QObject
-      # @!method show
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Shows the widget and its child widgets.
+      # Shows the widget and its child widgets.
       #
-      #   For child windows, this is equivalent to calling setVisible(true).
-      #   Otherwise, it is equivalent to calling showFullScreen(),
-      #   showMaximized(), or setVisible(true), depending on the platform's
-      #   default behavior for the window flags.
+      # For child windows, this is equivalent to calling setVisible(true).
+      # Otherwise, it is equivalent to calling showFullScreen(),
+      # showMaximized(), or setVisible(true), depending on the platform's
+      # default behavior for the window flags.
+      def show
+        _show
+      end
 
-      # @!method show_full_screen
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Shows the widget in full-screen mode.
+      # Shows the widget in full-screen mode.
       #
-      #   Calling this function only affects windows.
+      # Calling this function only affects windows.
       #
-      #   To return from full-screen mode, call showNormal() or close().
+      # To return from full-screen mode, call showNormal() or close().
+      def show_full_screen
+        _show_full_screen
+      end
 
-      # @!method show_maximized
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Shows the widget maximized.
+      # Shows the widget maximized.
       #
-      #   Calling this function only affects windows.
+      # Calling this function only affects windows.
+      def show_maximized
+        _show_maximized
+      end
 
-      # @!method show_minimized
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Shows the widget minimized, as an icon.
+      # Shows the widget minimized, as an icon.
       #
-      #   Calling this function only affects windows.
+      # Calling this function only affects windows.
+      def show_minimized
+        _show_minimized
+      end
 
-      # @!method show_normal
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Restores the widget after it has been maximized or minimized.
+      # Restores the widget after it has been maximized or minimized.
       #
-      #   Calling this function only affects windows.
+      # Calling this function only affects windows.
+      def show_normal
+        _show_normal
+      end
 
-      # @!method hide
-      #   @return [nil]
+      # @return [nil]
       #
-      #   Hides the widget. This function is equivalent to setVisible(false).
+      # Hides the widget. This function is equivalent to setVisible(false).
+      def hide
+        _hide
+      end
 
-      # @!method size
-      #   @return [QSize]
+      # @return [QSize]
       #
-      #   This property holds the size of the widget excluding any window frame
+      # This property holds the size of the widget excluding any window frame
       #
-      #   If the widget is visible when it is being resized, it receives a resize
-      #   event (resizeEvent()) immediately. If the widget is not currently
-      #   visible, it is guaranteed to receive an event before it is shown.
+      # If the widget is visible when it is being resized, it receives a resize
+      # event (resizeEvent()) immediately. If the widget is not currently
+      # visible, it is guaranteed to receive an event before it is shown.
       #
-      #   The size is adjusted if it lies outside the range defined by
-      #   minimumSize() and maximumSize().
+      # The size is adjusted if it lies outside the range defined by
+      # minimumSize() and maximumSize().
       #
-      #   By default, this property contains a value that depends on the user's
-      #   platform and screen geomet
+      # By default, this property contains a value that depends on the user's
+      # platform and screen geomet
+      def size
+        _size
+      end
 
-      # @!method resize(..)
-      #   @return [nil]
-      #   @overload rezie(width, height)
-      #     @param width [Integer]
-      #     @param height [Integer]
-      #   @overload rezie(size)
-      #     @param size [QSize]
+      # @param size [QSize]
+      # @return [nil]
       #
-      #   Sets the widget size.
+      # Sets the widget size.
+      def resize(size)
+        _resize(size)
+      end
 
-      # @!method font
-      #   @return [QFont]
+      # @return [QFont]
       #
-      #   This property holds the font currently set for the widget
+      # This property holds the font currently set for the widget
       #
-      #   This property describes the widget's requested font. The font is used
-      #   by the widget's style when rendering standard components, and is
-      #   available as a means to ensure that custom widgets can maintain
-      #   consistency with the native platform's look and feel. It's common
-      #   that different platforms, or different styles, define different
-      #   fonts for an application.
+      # This property describes the widget's requested font. The font is used
+      # by the widget's style when rendering standard components, and is
+      # available as a means to ensure that custom widgets can maintain
+      # consistency with the native platform's look and feel. It's common
+      # that different platforms, or different styles, define different
+      # fonts for an application.
       #
-      #   When you assign a new font to a widget, the properties from this font
-      #   are combined with the widget's default font to form the widget's final
-      #   font. You can call fontInfo() to get a copy of the widget's final font.
-      #   The final font is also used to initialize QPainter's font.
+      # When you assign a new font to a widget, the properties from this font
+      # are combined with the widget's default font to form the widget's final
+      # font. You can call fontInfo() to get a copy of the widget's final font.
+      # The final font is also used to initialize QPainter's font.
       #
-      #   The default depends on the system environment. QApplication maintains
-      #   a system/theme font which serves as a default for all widgets. There
-      #   may also be special font defaults for certain types of widgets. You
-      #   can also define default fonts for widgets yourself by passing a custom
-      #   font and the name of a widget to QApplication::setFont(). Finally, the
-      #   font is matched against Qt's font database to find the best match.
+      # The default depends on the system environment. QApplication maintains
+      # a system/theme font which serves as a default for all widgets. There
+      # may also be special font defaults for certain types of widgets. You
+      # can also define default fonts for widgets yourself by passing a custom
+      # font and the name of a widget to QApplication::setFont(). Finally, the
+      # font is matched against Qt's font database to find the best match.
       #
-      #   QWidget propagates explicit font properties from parent to child. If
-      #   you change a specific property on a font and assign that font to a
-      #   widget, that property will propagate to all the widget's children,
-      #   overriding any system defaults for that property. Note that fonts by
-      #   default don't propagate to windows (see isWindow()) unless the
-      #   Qt::WA_WindowPropagation attribute is enabled.
+      # QWidget propagates explicit font properties from parent to child. If
+      # you change a specific property on a font and assign that font to a
+      # widget, that property will propagate to all the widget's children,
+      # overriding any system defaults for that property. Note that fonts by
+      # default don't propagate to windows (see isWindow()) unless the
+      # Qt::WA_WindowPropagation attribute is enabled.
       #
-      #   QWidget's font propagation is similar to its palette propagation.
+      # QWidget's font propagation is similar to its palette propagation.
       #
-      #   The current style, which is used to render the content of all standard
-      #   Qt widgets, is free to choose to use the widget font, or in some cases,
-      #   to ignore it (partially, or completely). In particular, certain styles
-      #   like GTK style, Mac style, and Windows Vista style, apply special
-      #   modifications to the widget font to match the platform's native look
-      #   and feel. Because of this, assigning properties to a widget's font is
-      #   not guaranteed to change the appearance of the widget. Instead, you
-      #   may choose to apply a styleSheet.
+      # The current style, which is used to render the content of all standard
+      # Qt widgets, is free to choose to use the widget font, or in some cases,
+      # to ignore it (partially, or completely). In particular, certain styles
+      # like GTK style, Mac style, and Windows Vista style, apply special
+      # modifications to the widget font to match the platform's native look
+      # and feel. Because of this, assigning properties to a widget's font is
+      # not guaranteed to change the appearance of the widget. Instead, you
+      # may choose to apply a styleSheet.
+      def font
+        _font
+      end
 
-      # @!method font=(font)
-      #   @param font [QFont]
-      #   @return [void]
+      # @param font [QFont]
+      # @return [nil]
       #
-      #   Sets the widget font.
-
-      # @!visibility private
-      def _yardoc
+      # Sets the widget font.
+      def font=(font)
+        _set_font(font)
       end
     end
   end
