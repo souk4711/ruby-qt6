af4a6209985213546e08f5610e2fbafcdf643fe3
John Doe <johndoe@example.com>
2025-08-27 03:55:58 +0800

    docs: Update qtbindings-turorial

--- examples/qtbindings-tutorials/02/main.rb
+++ examples/qtbindings-tutorials/02/main.rb
@@ -3,7 +3,7 @@
 require 'qt6/all'
 
 a = QApplication.new
-quit = QPushButton.new('Quit', nil)
+quit = QPushButton.new('Quit')
 quit.resize(75, 30)
 quit.font = QFont.new('Times', 18, QFont::Bold)
 QObject.connect(quit, SIGNAL('clicked()'), a, SLOT('quit()'))
---
+++ examples/qtbindings-tutorials/03/main.rb
@@ -0,0 +1,17 @@
+require 'qt6/all'
+
+app = QApplication.new
+
+window = QWidget.new
+window.resize(200, 120)
+
+quit = QPushButton.new('Quit', window)
+quit.font = QFont.new('Times', 18, QFont::Bold)
+quit.geometry = QRect.new(10, 40, 180, 40)
+QObject.connect(quit, SIGNAL('clicked()'), app, SLOT('quit()'))
+
+window.show
+app.exec
+
+# FIXME: QPushButton double free ?
+quit._set_parent(nil)
