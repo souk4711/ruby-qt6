49c81f12a9add5294a73022e5a7acdbceedeb3df
John Doe <johndoe@example.com>
2025-08-29 01:08:03 +0800

    chore: Update bindgen task

--- bindgen/ruby-bindgen-patch.rb
+++ bindgen/ruby-bindgen-patch.rb
@@ -4,8 +4,6 @@ class RubyBindgen::Namer
     if cursor.kind == :cursor_cxx_method
       if cursor.spelling.match(/^operator/)
         _ruby(cursor)
-      elsif cursor.spelling.match(/^[iI]s[A-Z_]/) && cursor.type.result_type.spelling == "bool"
-        "_#{cursor.spelling.underscore.sub(/^is_/, "")}?"
       else
         "_#{_ruby(cursor)}"
       end
