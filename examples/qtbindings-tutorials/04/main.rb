bbe3fe24fd8400bd56dd25d42405335fb9c7967a
John Doe <johndoe@example.com>
2025-08-30 17:12:26 +0800

    docs: Update examples

--- examples/qtbindings-tutorials/04/main.rb
+++ examples/qtbindings-tutorials/04/main.rb
@@ -3,18 +3,18 @@
 require 'qt6/all'
 
 class MyWidget < QWidget
-  def initialize(parent: nil, app:)
-    super(parent)
+  def initialize
+    super
     set_fixed_size(200, 120)
 
     quit = QPushButton.new('Quit', self)
     quit.set_font(QFont.new('Times', 18, QFont::Bold))
     quit.set_geometry(62, 40, 75, 30)
-    QObject.connect(quit, SIGNAL('clicked()'), app, SLOT('quit()'))
+    QObject.connect(quit, SIGNAL('clicked()'), $qApp, SLOT('quit()'))
   end
 end
 
 app = QApplication.new
-widget = MyWidget.new(app:)
+widget = MyWidget.new
 widget.show
 app.exec
