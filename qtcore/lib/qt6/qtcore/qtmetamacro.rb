91c05f14f84f2da4452eef00a78a7a51627573d4
John Doe <johndoe@example.com>
2025-09-04 05:37:21 +0800

    feat: Update QtCore

--- qtcore/lib/qt6/qtcore/qtmetamacro.rb
+++ qtcore/lib/qt6/qtcore/qtmetamacro.rb
@@ -4,10 +4,14 @@ module RubyQt6
   module QtCore
     # @see https://doc.qt.io/qt-6/signalsandslots.html
     module QtMetaMacro
+      # @param signature [String]
+      # @return [String]
       def SIGNAL(signature)
         "2#{signature}"
       end
 
+      # @param signature [String]
+      # @return [String]
       def SLOT(signature)
         "1#{signature}"
       end
