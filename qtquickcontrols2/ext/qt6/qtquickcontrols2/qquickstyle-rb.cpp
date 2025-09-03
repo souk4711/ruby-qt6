1ad10baa4c8c923fd05f90c92c4ed24ef5cf5d0f
John Doe <johndoe@example.com>
2025-09-04 05:20:13 +0800

    feat: Update QtQuickControls2

--- qtquickcontrols2/ext/qt6/qtquickcontrols2/qquickstyle-rb.cpp
+++ qtquickcontrols2/ext/qt6/qtquickcontrols2/qquickstyle-rb.cpp
@@ -10,7 +10,7 @@ void Init_qquickstyle(Rice::Module rb_mQt6QtQuickControls2)
     rb_cQQuickStyle =
         // RubyQt6::QtQuickControls2::QQuickStyle
         define_class_under<QQuickStyle>(rb_mQt6QtQuickControls2, "QQuickStyle")
-            .define_singleton_function("_name", &QQuickStyle::name)
-            .define_singleton_function("_set_style", &QQuickStyle::setStyle, Arg("style"))
-            .define_singleton_function("_set_fallback_style", &QQuickStyle::setFallbackStyle, Arg("style"));
+            .define_singleton_function("name", &QQuickStyle::name)
+            .define_singleton_function("set_style", &QQuickStyle::setStyle, Arg("style"))
+            .define_singleton_function("set_fallback_style", &QQuickStyle::setFallbackStyle, Arg("style"));
 }
--- qtquickcontrols2/lib/qt6/qtquickcontrols2/qquickstyle.rb
+++ qtquickcontrols2/lib/qt6/qtquickcontrols2/qquickstyle.rb
@@ -4,28 +4,6 @@ module RubyQt6
   module QtQuickControls2
     # @see https://doc.qt.io/qt-6/qquickstyle.html
     class QQuickStyle
-      # @return [QString]
-      #
-      # Returns the name of the application style.
-      def self.name
-        _name
-      end
-
-      # @param style [QString]
-      # @return [nil]
-      #
-      # Sets the application style.
-      def self.set_style(style)
-        _set_style(QtCore::QString.new(style))
-      end
-
-      # @param style [QString]
-      # @return [nil]
-      #
-      # Sets the application fallback style.
-      def self.set_fallback_style(style)
-        _set_fallback_style(QtCore::QString.new(style))
-      end
     end
   end
 end
