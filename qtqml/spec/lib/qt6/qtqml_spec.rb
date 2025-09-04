eaa7054c02e86aca8f46c5505cf6c2795875cc9b
John Doe <johndoe@example.com>
2025-09-05 03:53:43 +0800

    test: Init rspec

---
+++ qtqml/spec/lib/qt6/qtqml_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe RubyQt6::QtQml do
+  Dir.glob("ext/**/*.cpp").each do |f|
+    contents = File.read(f)
+    matched = contents.match(/define_constructor\(Constructor<(\w+)/)
+    next unless matched
+
+    it "redefine #initialize for #{matched[1]}" do
+      contents = File.read("lib/qt6/qtqml/#{matched[1].downcase}.rb")
+      expect(contents.match("alias_method :_initialize, :initialize")).not_to be_nil
+      expect(contents.match("def initialize")).not_to be_nil
+    end
+  end
+end
