3f5225fa5b0bdbb72c9bc5e15f6d668e793a6be3
John Doe <johndoe@example.com>
2025-09-04 23:39:19 +0800

    feat: Update QtQuick

--- qtquick/ext/qt6/qtquick/qquickview-rb.cpp
+++ qtquick/ext/qt6/qtquick/qquickview-rb.cpp
@@ -16,21 +16,22 @@ void Init_qquickview(Rice::Module rb_mQt6QtQuick)
         define_class_under<QQuickView, QQuickWindow>(rb_mQt6QtQuick, "QQuickView")
             // Constructor
             .define_constructor(Constructor<QQuickView, QWindow *>(), Arg("parent") = static_cast<QWindow *>(nullptr))
-            // Properties
-            .define_method("resize_mode", &QQuickView::resizeMode)
-            .define_method("source", &QQuickView::source)
-            .define_method("status", &QQuickView::status)
-            .define_method("set_resize_mode", &QQuickView::setResizeMode, Arg("mode"))
-            .define_method("set_source", &QQuickView::setSource, Arg("source"))
             // Public Functions
             .define_method("engine", &QQuickView::engine)
             .define_method("errors", &QQuickView::errors)
             .define_method("initial_size", &QQuickView::initialSize)
+            .define_method("resize_mode", &QQuickView::resizeMode)
             .define_method("root_context", &QQuickView::rootContext)
             .define_method("root_object", &QQuickView::rootObject)
+            .define_method("set_content", &QQuickView::setContent, Arg("url"), Arg("component"), Arg("item"))
+            .define_method("set_resize_mode", &QQuickView::setResizeMode, Arg(""))
+            .define_method("size_hint", &QQuickView::sizeHint)
+            .define_method("source", &QQuickView::source)
+            .define_method("status", &QQuickView::status)
             // Public Slots
             .define_method("load_from_module", &QQuickView::loadFromModule, Arg("uri"), Arg("type_name"))
             .define_method("set_initial_properties", &QQuickView::setInitialProperties, Arg("initial_properties"))
+            .define_method("set_source", &QQuickView::setSource, Arg(""))
             // Signals
-            .define_method("status_changed", &QQuickView::statusChanged, Arg("status"));
+            .define_method("status_changed", &QQuickView::statusChanged, Arg(""));
 }
--- qtquickcontrols2/ext/qt6/qtquickcontrols2/qquickstyle-rb.cpp
+++ qtquickcontrols2/ext/qt6/qtquickcontrols2/qquickstyle-rb.cpp
@@ -10,6 +10,7 @@ void Init_qquickstyle(Rice::Module rb_mQt6QtQuickControls2)
     rb_cQQuickStyle =
         // RubyQt6::QtQuickControls2::QQuickStyle
         define_class_under<QQuickStyle>(rb_mQt6QtQuickControls2, "QQuickStyle")
+            // Static Public Members
             .define_singleton_function("name", &QQuickStyle::name)
             .define_singleton_function("set_style", &QQuickStyle::setStyle, Arg("style"))
             .define_singleton_function("set_fallback_style", &QQuickStyle::setFallbackStyle, Arg("style"));
