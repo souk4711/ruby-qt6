cb89df1417e8d197951e23f8eedd1fb2ee360a6c
John Doe <johndoe@example.com>
2025-09-06 00:22:37 +0800

    feat: Update QQmlEngine

--- qtqml/lib/qt6/qtqml/qqmlengine.rb
+++ qtqml/lib/qt6/qtqml/qqmlengine.rb
@@ -13,6 +13,11 @@ module RubyQt6
         _initialize(parent)
         _take_ownership_from_rubyrice(self) if parent
       end
+
+      # @!visibility private
+      def add_import_path(dir)
+        _add_import_path(QtCore::QString.new(dir))
+      end
     end
   end
 end
