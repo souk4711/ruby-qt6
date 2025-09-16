6c57cbffc927d43719fc54dccc715561994720e6
John Doe <johndoe@example.com>
2025-09-17 05:40:00 +0800

    test: Fix broken testcases

--- qtcore/spec/lib/qt6/qtcore_spec.rb
+++ qtcore/spec/lib/qt6/qtcore_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe RubyQt6::QtCore do
     next unless matched
 
     klass = matched[1]
+    next if klass.start_with?("Bando_")
     next if klass == "QMetaObjectBuilder"
 
     it "redefine #initialize for #{klass}" do
       contents = File.read("lib/qt6/qtcore/#{klass.downcase}.rb")
-      contents = File.read("lib/qt6/qtcore/qobject/base.rb") if klass == "QObject"
       expect(contents.match("alias_method :_initialize, :initialize")).not_to be_nil
       expect(contents.match("def initialize")).not_to be_nil
     end
