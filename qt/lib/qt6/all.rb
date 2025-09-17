b129713b2cdc3b98135e111337552075c14b3573
John Doe <johndoe@example.com>
2025-09-18 01:30:16 +0800

    feat: Import Bando qlass

--- qt/lib/qt6/all.rb
+++ qt/lib/qt6/all.rb
@@ -7,9 +7,17 @@ require "qt6/qtquick"
 require "qt6/qtquickcontrols2"
 require "qt6/qtwidgets"
 
-rubyqt6_include_constants Object, RubyQt6::QtCore
-rubyqt6_include_constants Object, RubyQt6::QtGui
-rubyqt6_include_constants Object, RubyQt6::QtQml
-rubyqt6_include_constants Object, RubyQt6::QtQuick
-rubyqt6_include_constants Object, RubyQt6::QtQuickControls2
-rubyqt6_include_constants Object, RubyQt6::QtWidgets
+[
+  RubyQt6::Bando,
+  RubyQt6::QtCore,
+  RubyQt6::QtGui,
+  RubyQt6::QtQml,
+  RubyQt6::QtQuick,
+  RubyQt6::QtQuickControls2,
+  RubyQt6::QtWidgets
+].each do |mod|
+  mod.constants.each do |const|
+    next if ::Object.const_defined?(const)
+    ::Object.const_set(const, mod.const_get(const)) if const.start_with?("Q")
+  end
+end
