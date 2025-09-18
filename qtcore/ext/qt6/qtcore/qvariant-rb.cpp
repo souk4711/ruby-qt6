876c69a841443b5a92a7854e117b3ccc7c8c040e
John Doe <johndoe@example.com>
2025-09-19 01:35:10 +0800

    feat: Update QVariant

--- qtcore/ext/qt6/qtcore/qvariant-rb.cpp
+++ qtcore/ext/qt6/qtcore/qvariant-rb.cpp
@@ -1,6 +1,38 @@
 #include "qvariant-rb.hpp"
 #include <qvariant.h>
 
+#include <QMetaType>
+#include <QBitArray>
+#include <QByteArray>
+#include <QChar>
+#include <QDate>
+#include <QDateTime>
+#include <QEasingCurve>
+#include <QHash>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QLine>
+#include <QLineF>
+#include <QList>
+#include <QLocale>
+#include <QMap>
+#include <QModelIndex>
+#include <QPersistentModelIndex>
+#include <QPoint>
+#include <QPointF>
+#include <QRect>
+#include <QRectF>
+#include <QRegularExpression>
+#include <QSize>
+#include <QSizeF>
+#include <QString>
+#include <QStringList>
+#include <QTime>
+#include <QUrl>
+#include <QUuid>
+
 using namespace Rice;
 
 Rice::Class rb_cQVariant;
@@ -9,5 +41,69 @@ void Init_qvariant(Rice::Module rb_mQt6QtCore)
 {
     rb_cQVariant =
         // RubyQt6::QtCore::QVariant
-        define_class_under<QVariant>(rb_mQt6QtCore, "QVariant");
+        define_class_under<QVariant>(rb_mQt6QtCore, "QVariant")
+            // Public Functions
+            .define_method<bool (QVariant::*)(QMetaType) const>("can_convert", &QVariant::canConvert, Arg("target_type"))
+            // .define_method("can_view", &QVariant::canView, Arg("target_type"))
+            .define_method("clear", &QVariant::clear)
+            .define_method("const_data", &QVariant::constData)
+            .define_method<bool (QVariant::*)(QMetaType)>("convert", &QVariant::convert, Arg("type"))
+            .define_method<const void *(QVariant::*)() const>("data", &QVariant::data)
+            .define_method<void *(QVariant::*)()>("data", &QVariant::data)
+            .define_method<QVariant::DataPtr &(QVariant::*)()>("data_ptr", &QVariant::data_ptr)
+            .define_method<const QVariant::DataPtr &(QVariant::*)() const>("data_ptr", &QVariant::data_ptr)
+            .define_method("detach", &QVariant::detach)
+            .define_method("detached?", &QVariant::isDetached)
+            .define_method("null?", &QVariant::isNull)
+            .define_method("valid?", &QVariant::isValid)
+            .define_method("load", &QVariant::load, Arg("ds"))
+            .define_method("meta_type", &QVariant::metaType)
+            .define_method("save", &QVariant::save, Arg("ds"))
+            .define_method<void (QVariant::*)(QVariant &&)>("set_value", &QVariant::setValue, Arg("avalue"))
+            .define_method<void (QVariant::*)(const QVariant &)>("set_value", &QVariant::setValue, Arg("avalue"))
+            .define_method("swap", &QVariant::swap, Arg("other"))
+            .define_method("to_bit_array", &QVariant::toBitArray)
+            .define_method("to_bool", &QVariant::toBool)
+            .define_method("to_byte_array", &QVariant::toByteArray)
+            .define_method("to_char", &QVariant::toChar)
+            .define_method("to_date", &QVariant::toDate)
+            .define_method("to_date_time", &QVariant::toDateTime)
+            .define_method("to_double", &QVariant::toDouble, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_easing_curve", &QVariant::toEasingCurve)
+            .define_method("to_float", &QVariant::toFloat, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_hash", &QVariant::toHash)
+            .define_method("to_int", &QVariant::toInt, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_json_array", &QVariant::toJsonArray)
+            .define_method("to_json_document", &QVariant::toJsonDocument)
+            .define_method("to_json_object", &QVariant::toJsonObject)
+            .define_method("to_json_value", &QVariant::toJsonValue)
+            .define_method("to_line", &QVariant::toLine)
+            .define_method("to_line_f", &QVariant::toLineF)
+            .define_method("to_list", &QVariant::toList)
+            .define_method("to_locale", &QVariant::toLocale)
+            .define_method("to_long_long", &QVariant::toLongLong, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_map", &QVariant::toMap)
+            .define_method("to_model_index", &QVariant::toModelIndex)
+            .define_method("to_persistent_model_index", &QVariant::toPersistentModelIndex)
+            .define_method("to_point", &QVariant::toPoint)
+            .define_method("to_point_f", &QVariant::toPointF)
+            .define_method("to_real", &QVariant::toReal, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_rect", &QVariant::toRect)
+            .define_method("to_rect_f", &QVariant::toRectF)
+            .define_method("to_regular_expression", &QVariant::toRegularExpression)
+            .define_method("to_size", &QVariant::toSize)
+            .define_method("to_size_f", &QVariant::toSizeF)
+            .define_method("to_string", &QVariant::toString)
+            .define_method("to_string_list", &QVariant::toStringList)
+            .define_method("to_time", &QVariant::toTime)
+            .define_method("to_u_int", &QVariant::toUInt, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_u_long_long", &QVariant::toULongLong, Arg("ok") = static_cast<bool *>(nullptr))
+            .define_method("to_url", &QVariant::toUrl)
+            .define_method("to_uuid", &QVariant::toUuid)
+            .define_method("type_id", &QVariant::typeId)
+            .define_method("type_name", &QVariant::typeName)
+            .define_method("user_type", &QVariant::userType)
+            // Static Public Members
+            .define_singleton_function("compare", &QVariant::compare, Arg("lhs"), Arg("rhs"))
+            .define_singleton_function("from_meta_type", &QVariant::fromMetaType, Arg("type"), Arg("copy") = static_cast<const void *>(nullptr));
 }
