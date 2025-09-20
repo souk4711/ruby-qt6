c03b765657f273e30d4b35df0735e902dc751a87
John Doe <johndoe@example.com>
2025-09-20 11:55:11 +0800

    docs: Update examples

---
+++ examples/qtbindings-tutorials/07/lcdrange.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'qt6/all'
+
+class LCDRange < QWidget
+  q_object do
+    signal 'value_changed(int)'
+    slot 'set_value(int)'
+  end
+
+  def initialize(parent = nil)
+    super
+
+    lcd = QLCDNumber.new(2)
+
+    @slider = QSlider.new(Qt::Horizontal)
+    @slider.set_range(0, 99)
+    @slider.set_value(0)
+    @slider.value_changed.connect(lcd, :display)
+    @slider.value_changed.connect(self, :value_changed)
+
+    layout = QVBoxLayout.new
+    layout.add_widget(lcd)
+    layout.add_widget(@slider)
+    set_layout(layout)
+  end
+
+  def value
+    @slider.value
+  end
+
+  def set_value(value)
+    @slider.set_value(value)
+  end
+end
---
+++ examples/qtbindings-tutorials/07/main.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'qt6/all'
+require_relative 'lcdrange'
+
+class MyWidget < QWidget
+  def initialize(parent = nil)
+    super(parent)
+
+    quit = QPushButton.new('Quit')
+    quit.set_font(QFont.new('Times', 18, QFont::Bold))
+    quit.clicked.connect($qApp, :quit)
+
+    grid = QGridLayout.new
+    previous_range = nil
+    (0..3).each do |row|
+      (0..3).each do |column|
+        lcd_range = LCDRange.new(self)
+        grid.add_widget(lcd_range, row, column)
+        lcd_range.value_changed.connect(previous_range, :set_value) unless previous_range.nil?
+        previous_range = lcd_range
+      end
+    end
+
+    layout = QVBoxLayout.new
+    layout.add_widget(quit)
+    layout.add_layout(grid)
+    set_layout(layout)
+  end
+end
+
+app = QApplication.new(ARGV)
+widget = MyWidget.new
+widget.show
+app.exec
