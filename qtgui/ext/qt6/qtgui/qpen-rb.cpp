d8ebf055abb0a447fcd745775d6fc115f14b02f3
John Doe <johndoe@example.com>
2025-09-22 05:42:55 +0800

    feat: Update QPen

--- qtgui/ext/qt6/qtgui/qpen-rb.cpp
+++ qtgui/ext/qt6/qtgui/qpen-rb.cpp
@@ -9,5 +9,34 @@ void Init_qpen(Rice::Module rb_mQt6QtGui)
 {
     rb_cQPen =
         // RubyQt6::QtGui::QPen
-        define_class_under<QPen>(rb_mQt6QtGui, "QPen");
+        define_class_under<QPen>(rb_mQt6QtGui, "QPen")
+            // Constructor
+            .define_constructor(Constructor<QPen, Qt::PenStyle>(), Arg("style"))
+            .define_constructor(Constructor<QPen, const QColor &>(), Arg("color"))
+            .define_constructor(Constructor<QPen, const QBrush &, qreal, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle>(), Arg("brush"), Arg("width"), Arg("s") = static_cast<Qt::PenStyle>(Qt::SolidLine), Arg("c") = static_cast<Qt::PenCapStyle>(Qt::SquareCap), Arg("j") = static_cast<Qt::PenJoinStyle>(Qt::BevelJoin))
+            // Public Functions
+            .define_method("brush", &QPen::brush)
+            .define_method("cap_style", &QPen::capStyle)
+            .define_method("color", &QPen::color)
+            .define_method("dash_offset", &QPen::dashOffset)
+            .define_method("dash_pattern", &QPen::dashPattern)
+            .define_method("cosmetic?", &QPen::isCosmetic)
+            .define_method("detached?", &QPen::isDetached)
+            .define_method("solid?", &QPen::isSolid)
+            .define_method("join_style", &QPen::joinStyle)
+            .define_method("miter_limit", &QPen::miterLimit)
+            .define_method("set_brush", &QPen::setBrush, Arg("brush"))
+            .define_method("set_cap_style", &QPen::setCapStyle, Arg("pcs"))
+            .define_method("set_color", &QPen::setColor, Arg("color"))
+            .define_method("set_cosmetic", &QPen::setCosmetic, Arg("cosmetic"))
+            .define_method("set_dash_offset", &QPen::setDashOffset, Arg("doffset"))
+            .define_method("set_dash_pattern", &QPen::setDashPattern, Arg("pattern"))
+            .define_method("set_join_style", &QPen::setJoinStyle, Arg("pcs"))
+            .define_method("set_miter_limit", &QPen::setMiterLimit, Arg("limit"))
+            .define_method("set_style", &QPen::setStyle, Arg(""))
+            .define_method("set_width", &QPen::setWidth, Arg("width"))
+            .define_method("set_width_f", &QPen::setWidthF, Arg("width"))
+            .define_method("style", &QPen::style)
+            .define_method("width", &QPen::width)
+            .define_method("width_f", &QPen::widthF);
 }
--- qtgui/lib/qt6/qtgui/qpen.rb
+++ qtgui/lib/qt6/qtgui/qpen.rb
@@ -4,6 +4,26 @@ module RubyQt6
   module QtGui
     # @see https://doc.qt.io/qt-6/qpen.html
     class QPen
+      # @!visibility private
+      alias_method :_initialize, :initialize
+
+      # @return [QPen]
+      #
+      # @overload initialize(style)
+      #   @param style [Qt::PenStyle]
+      #
+      # @overload initialize(color)
+      #   @param color [QColor]
+      #
+      # @overload initialize(brush, width, s = Qt::SolidLine, c = Qt::SquareCap, j = Qt::BevelJoin)
+      #   @param brush [QBrush]
+      #   @param width [Float]
+      #   @param s [Qt::PenStyle]
+      #   @param c [Qt::PenCapStyle]
+      #   @param j [Qt::PenJoinStyle]
+      def initialize(*args)
+        _initialize(*args)
+      end
     end
   end
 end
