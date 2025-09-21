99404686922d57b0d63ebbf40b8c1993449a643b
John Doe <johndoe@example.com>
2025-09-22 06:24:00 +0800

    feat: Update QDateTime

--- qtcore/lib/qt6/qtcore/qdatetime.rb
+++ qtcore/lib/qt6/qtcore/qdatetime.rb
@@ -4,6 +4,24 @@ module RubyQt6
   module QtCore
     # @see https://doc.qt.io/qt-6/qdatetime.html
     class QDateTime
+      # @!visibility private
+      alias_method :_initialize, :initialize
+
+      # @return [QDateTime]
+      #
+      # @overload initialize(date, time, time_zone, resolve = QDateTime::LegacyBehavior)
+      #   @param date [QDate]
+      #   @param time [QTime]
+      #   @param time_zone [QTimeZone]
+      #   @param resolve [QDateTime::TransitionResolution]
+      #
+      # @overload initialize(date, time, resolve = QDateTime::LegacyBehavior)
+      #   @param date [QDate]
+      #   @param time [QTime]
+      #   @param resolve [QDateTime::TransitionResolution]
+      def initialize(*args)
+        _initialize(*args)
+      end
     end
   end
 end
