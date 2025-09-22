7f0bb9a212ff7947febb552d913f7878f5b7e8b4
John Doe <johndoe@example.com>
2025-09-23 06:25:56 +0800

    test: Update spec helper

--- qtcore/lib/qt6/rspec.rb
+++ qtcore/lib/qt6/rspec.rb
@@ -6,10 +6,17 @@ require_relative "rspec/qlass_file_parser"
 
 module RubyQt6
   module RSpec
-    NO_VERIFY_INITIALIZE = QlassFileParser::NESTED_QLASSES + [
+    NO_VERIFY_QLASS_INITIALIZE = QlassFileParser::NESTED_QLASSES + [
       "QStackedLayout",
       "QVariant"
     ]
+    NO_VERIFY_QLASS_INITIALIZE_YARDOC = [
+      "QBrush",
+      "QColor"
+    ]
+    NO_VERIFY_QLASS_LEADING_UNDERSCORE_METHODS = {
+      "QObject" => ["_connect", "_disconnect"]
+    }
 
     def self.inflector
       @inflector ||= ::Dry::Inflector.new do |inflections|
@@ -28,21 +35,28 @@ module RubyQt6
       parser = QlassFileParser.new(cppfile, qmod)
       qlasses = parser.parse
 
+      rs = {}
       qlasses.each do |qlass|
+        r = OpenStruct.new(qlass:)
+        rs[qlass.name] = r
+
         rbfile = "lib/qt6/#{qmod.name.downcase}/#{qlass.name.downcase}.rb"
         rbfile_contents = File.read(rbfile) if File.exist?(rbfile)
 
-        unless NO_VERIFY_INITIALIZE.include?(qlass.name)
+        unless NO_VERIFY_QLASS_INITIALIZE.include?(qlass.name)
           constructor_methods = qlass.methods.select { |method| method.type == :constructor }
           case constructor_methods.size
           when 2..99
+            raise rbfile unless NO_VERIFY_QLASS_INITIALIZE_YARDOC.include?(qlass.name) || rbfile_contents.scan("# @overload initialize").count == constructor_methods.size
             raise rbfile unless rbfile_contents.include?("alias_method :_initialize, :initialize")
             raise rbfile unless rbfile_contents.include?("def initialize(*args)")
             raise rbfile unless rbfile_contents.include?("_initialize(*args)")
+            r.verified_initialize = 2
           when 1
             raise rbfile unless rbfile_contents.include?("alias_method :_initialize, :initialize")
             raise rbfile unless rbfile_contents.include?("def initialize")
             raise rbfile unless rbfile_contents.include?("_initialize")
+            r.verified_initialize = 1
           end
         end
 
@@ -69,8 +83,30 @@ module RubyQt6
               raise method.inspect
             end
           end
+
+          # No unknonwn leading underscore method
+          if method.rbname.start_with?("_")
+            lambda do
+              return if method.type == :rubyqt6_defined_functions
+              return if rbfile_contents.include?("def #{method.rbname.sub(/^_/, "")}")
+              return if NO_VERIFY_QLASS_LEADING_UNDERSCORE_METHODS[qlass.name]&.include?(method.rbname)
+              raise method.inspect
+            end.call
+          end
+
+          r.verified_methods_count ||= 0
+          r.verified_methods_count += 1
+        end
+
+        qlass.enums.each do |enum|
+          raise rbfile unless rbfile_contents.include?("# @!parse class #{enum.name}")
+          raise rbfile unless rbfile_contents.include?("rubyqt6_include_constants #{qlass.name}, #{qlass.name}::#{enum.name}")
+          r.verified_enums_count ||= 0
+          r.verified_enums_count += 1
         end
       end
+
+      rs
     end
   end
 end
---
+++ qtcore/spec/lib/qt6/rspec_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe RubyQt6::RSpec do
+  describe ".verify_qlass_cppfile" do
+    it "ext/qt6/qtcore/qobject-rb.cpp" do
+      cppfile = "ext/qt6/qtcore/qobject-rb.cpp"
+      qmod = OpenStruct.new(name: "QtCore")
+      rs = RubyQt6::RSpec.verify_qlass_cppfile cppfile, qmod
+
+      r = rs["QObject"]
+      expect(r.verified_initialize).to eq(1)
+      expect(r.verified_methods_count).to eq(45)
+      expect(r.verified_enums_count).to eq(nil)
+
+      qlass = r.qlass
+      qlass_methods = qlass.methods.group_by(&:type)
+      expect(qlass_methods[:rubyqt6_defined_functions].count).to eq(3)
+      expect(qlass_methods[:constructor].count).to eq(1)
+      expect(qlass_methods[:inherits]).to eq(nil)
+      expect(qlass_methods[:public_functions].count).to eq(33)
+      expect(qlass_methods[:public_slots].count).to eq(1)
+      expect(qlass_methods[:signals].count).to eq(2)
+      expect(qlass_methods[:static_public_members].count).to eq(5)
+
+      method = qlass.methods.find { |method| method.rbname == "set_object_name" }
+      expect(method.cppname).to eq("setObjectName")
+    end
+
+    it "ext/qt6/qtcore/qmetamethod-rb.cpp" do
+      cppfile = "ext/qt6/qtcore/qmetamethod-rb.cpp"
+      qmod = OpenStruct.new(name: "QtCore")
+      rs = RubyQt6::RSpec.verify_qlass_cppfile cppfile, qmod
+
+      r = rs["QMetaMethod"]
+      expect(r.verified_initialize).to eq(nil)
+      expect(r.verified_methods_count).to eq(29)
+      expect(r.verified_enums_count).to eq(3)
+
+      qlass = r.qlass
+      qlass_enums = qlass.enums
+      expect(qlass_enums.map(&:name)).to eq(["Access", "Attributes", "MethodType"])
+    end
+  end
+end
