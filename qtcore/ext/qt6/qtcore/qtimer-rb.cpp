71a76f605cdf5348d8d45611178985e5acac68df
John Doe <johndoe@example.com>
2025-09-23 07:52:21 +0800

    fix: Fix QTimer._qmetaobject

--- qtcore/ext/qt6/qtcore/qtimer-rb.cpp
+++ qtcore/ext/qt6/qtcore/qtimer-rb.cpp
@@ -12,6 +12,8 @@ void Init_qtimer(Rice::Module rb_mQt6QtCore)
     rb_cQTimer =
         // RubyQt6::QtCore::QTimer
         define_class_under<QTimer, QObject>(rb_mQt6QtCore, "QTimer")
+            // RubyQt6-Defined Functions
+            .define_singleton_function("_static_meta_object", []() -> const QMetaObject * { return &QTimer::staticMetaObject; })
             // Constructor
             .define_constructor(Constructor<QTimer, QObject *>(), Arg("parent"))
             // Public Functions
