aeb40bbe072e1cafbfec053c2ef6130414e15456
John Doe <johndoe@example.com>
2025-09-25 04:01:26 +0800

    feat: Update QIcon

--- qtgui/lib/qt6/qtgui/qicon.rb
+++ qtgui/lib/qt6/qtgui/qicon.rb
@@ -4,6 +4,27 @@ module RubyQt6
   module QtGui
     # @see https://doc.qt.io/qt-6/qicon.html
     class QIcon
+      # @!parse class Mode     ; end
+      # @!parse class State    ; end
+      # @!parse class ThemeIcon; end
+      rubyqt6_include_constants QIcon, QIcon::Mode
+      rubyqt6_include_constants QIcon, QIcon::State
+      rubyqt6_include_constants QIcon, QIcon::ThemeIcon
+
+      # @!visibility private
+      alias_method :_initialize, :initialize
+
+      # @return [QIcon]
+      #
+      # @overload initialize(filename)
+      #   @param filename [String, QString]
+      #
+      # @overload initialize(pixmap)
+      #   @param pixmap [QPixmap]
+      def initialize(*args)
+        args[0] = T.to_qstr(args[0]) if args[0].is_a?(String)
+        _initialize(*args)
+      end
     end
   end
 end
