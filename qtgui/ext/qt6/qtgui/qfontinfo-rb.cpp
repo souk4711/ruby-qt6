37db61ac7e587926fa00b056e4dfeb8b2eda3ec6
John Doe <johndoe@example.com>
2025-09-25 03:30:56 +0800

    feat: Update QFontInfo

--- qtgui/ext/qt6/qtgui/qfontinfo-rb.cpp
+++ qtgui/ext/qt6/qtgui/qfontinfo-rb.cpp
@@ -1,6 +1,8 @@
 #include "qfontinfo-rb.hpp"
 #include <qfontinfo.h>
 
+#include <QList>
+
 using namespace Rice;
 
 Rice::Class rb_cQFontInfo;
@@ -9,5 +11,25 @@ void Init_qfontinfo(Rice::Module rb_mQt6QtGui)
 {
     rb_cQFontInfo =
         // RubyQt6::QtGui::QFontInfo
-        define_class_under<QFontInfo>(rb_mQt6QtGui, "QFontInfo");
+        define_class_under<QFontInfo>(rb_mQt6QtGui, "QFontInfo")
+            // Constructor
+            .define_constructor(Constructor<QFontInfo, const QFont &>(), Arg("font"))
+            // Public Functions
+            .define_method("bold", &QFontInfo::bold)
+            .define_method("exact_match", &QFontInfo::exactMatch)
+            .define_method("family", &QFontInfo::family)
+            .define_method("fixed_pitch", &QFontInfo::fixedPitch)
+            .define_method("italic", &QFontInfo::italic)
+            .define_method("overline", &QFontInfo::overline)
+            .define_method("pixel_size", &QFontInfo::pixelSize)
+            .define_method("point_size", &QFontInfo::pointSize)
+            .define_method("point_size_f", &QFontInfo::pointSizeF)
+            .define_method("strike_out", &QFontInfo::strikeOut)
+            .define_method("style", &QFontInfo::style)
+            .define_method("style_hint", &QFontInfo::styleHint)
+            .define_method("style_name", &QFontInfo::styleName)
+            .define_method("swap", &QFontInfo::swap, Arg("other"))
+            .define_method("underline", &QFontInfo::underline)
+            .define_method("variable_axes", &QFontInfo::variableAxes)
+            .define_method("weight", &QFontInfo::weight);
 }
--- qtgui/lib/qt6/qtgui/qfontinfo.rb
+++ qtgui/lib/qt6/qtgui/qfontinfo.rb
@@ -4,6 +4,14 @@ module RubyQt6
   module QtGui
     # @see https://doc.qt.io/qt-6/qfontinfo.html
     class QFontInfo
+      # @!visibility private
+      alias_method :_initialize, :initialize
+
+      # @param font [QFont]
+      # @return [QFontInfo]
+      def initialize(font)
+        _initialize(font)
+      end
     end
   end
 end
