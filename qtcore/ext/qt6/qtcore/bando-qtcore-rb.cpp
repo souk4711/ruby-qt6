b5033b32571690d7168e16d606a8d6481f0fa5bd
John Doe <johndoe@example.com>
2025-09-24 22:09:49 +0800

    feat: Add Bando::QTimer

--- qtcore/ext/qt6/qtcore/bando-qtcore-rb.cpp
+++ qtcore/ext/qt6/qtcore/bando-qtcore-rb.cpp
@@ -3,14 +3,17 @@
 
 #include <QObject>
 #include <QCoreApplication>
+#include <QTimer>
 
 using namespace Rice;
 
 using Bando_QObject = BandoQObject<QObject, QObject *>;
 using Bando_QCoreApplication = BandoQObject<QCoreApplication, int &, char **>;
+using Bando_QTimer = BandoQObject<QTimer, QObject *>;
 
 Rice::Class rb_mBando_cQObject;
 Rice::Class rb_mBando_cQCoreApplication;
+Rice::Class rb_mBando_cQTimer;
 
 void Init_bando_qtcore(Rice::Module rb_mQt6Bando)
 {
@@ -23,4 +26,9 @@ void Init_bando_qtcore(Rice::Module rb_mQt6Bando)
         define_class_under<Bando_QCoreApplication, QCoreApplication>(rb_mQt6Bando, "QCoreApplication")
             .define_constructor(Constructor<Bando_QCoreApplication, int &, char **>(), Arg("argc"), Arg("argv"))
             .define_method("_initialize_value", &Bando_QCoreApplication::initializeValue, Arg("mo"), Arg("value"));
+
+    rb_mBando_cQTimer =
+        define_class_under<Bando_QTimer, QTimer>(rb_mQt6Bando, "QTimer")
+            .define_constructor(Constructor<Bando_QTimer, QObject *>(), Arg("parent"))
+            .define_method("_initialize_value", &Bando_QTimer::initializeValue, Arg("mo"), Arg("value"));
 }
---
+++ qtcore/lib/qt6/bando/qtimer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module RubyQt6
+  module Bando
+    class QTimer < RubyQt6::QtCore::QTimer
+      Bando.define_bando_qlass Bando::QTimer
+    end
+  end
+end
