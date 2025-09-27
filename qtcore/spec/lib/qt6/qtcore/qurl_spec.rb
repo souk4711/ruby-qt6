f74365ef249865ef972437da0d52487f2034a065
John Doe <johndoe@example.com>
2025-09-27 08:00:38 +0800

    test: Add QUrl testcase

---
+++ qtcore/spec/lib/qt6/qtcore/qurl_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe RubyQt6::QtCore::QUrl do
+  it "#initialize" do
+    o = described_class.new("https://github.com/souk4711/ruby-qt6/issues?q=is:issue state:open")
+    expect(o.scheme).to eq("https")
+    expect(o.host).to eq("github.com")
+    expect(o.path).to eq("/souk4711/ruby-qt6/issues")
+    expect(o.query).to eq("q=is:issue state:open")
+  end
+
+  it "#to_s" do
+    o = described_class.new("https://github.com/souk4711/ruby-qt6/issues?q=is:issue state:open")
+    expect(o.to_s).to eq("https://github.com/souk4711/ruby-qt6/issues?q=is:issue state:open")
+  end
+end
