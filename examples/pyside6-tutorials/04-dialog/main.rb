c51189c7d7a81ae185714a4512bde9a609cbd493
John Doe <johndoe@example.com>
2025-09-28 06:08:43 +0800

    docs: Update examples

---
+++ examples/pyside6-tutorials/04-dialog/main.rb
@@ -0,0 +1,29 @@
+require "qt6/all"
+
+class Form < QDialog
+  q_object do
+    slot "greetings()"
+  end
+
+  def initialize
+    super
+
+    @edit = QLineEdit.new("Write my name here")
+    @button = QPushButton.new("Show Greetings")
+    @button.clicked.connect(self, :greetings)
+
+    layout = QVBoxLayout.new
+    layout.add_widget(@edit)
+    layout.add_widget(@button)
+    set_layout(layout)
+  end
+
+  def greetings
+    puts "#{@edit.text}"
+  end
+end
+
+app = QApplication.new(ARGV)
+form = Form.new
+form.show
+app.exec
