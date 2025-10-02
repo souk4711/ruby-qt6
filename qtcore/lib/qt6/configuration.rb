ca2ffc72a1401c863c4bf233dafcc76e6e9ad728
John Doe <johndoe@example.com>
2025-10-03 02:46:19 +0800

    feat: Update RubyQt6.load_defaults

--- qtcore/lib/qt6/configuration.rb
+++ qtcore/lib/qt6/configuration.rb
@@ -5,17 +5,9 @@ require "semantic_logger/sync"
 module RubyQt6
   # @!visibility private
   def self.load_defaults
-    mods = [::RubyQt6::Bando]
-    mods.each do |mod|
-      mod.constants.each do |const|
-        ::Object.const_set(const, mod.const_get(const)) if const.start_with?("Q")
-      end
-    end
-
     mods = ::RubyQt6.constants.grep(/^Qt/).map { |const| ::RubyQt6.const_get(const) }
     mods.each do |mod|
       mod.constants.each do |const|
-        next if ::RubyQt6::Bando.const_defined?(const)
         next if const.match?("VERSION")
         ::Object.const_set(const, mod.const_get(const)) if const.start_with?("Q")
       end
