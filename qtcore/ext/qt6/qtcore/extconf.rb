2f0bf29f0c570041850572b99fb7f344391a8cfc
John Doe <johndoe@example.com>
2025-10-05 06:12:03 +0800

    fix: Update extconf.rb

--- qtcore/ext/qt6/qtcore/extconf.rb
+++ qtcore/ext/qt6/qtcore/extconf.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 require "mkmf-rice"
-require_relative "../../../lib/mkmf-rubyqt6"
+
+QTCORE_LIB_PATH = File.join(__dir__, "../../../../qtcore/lib")
+if File.exist?(QTCORE_LIB_PATH)
+  require File.join(QTCORE_LIB_PATH, "mkmf-rubyqt6.rb")
+else
+  require "mkmf-rubyqt6"
+end
 
 includedir = qt_install_headers
 includedir_private = Dir["#{includedir}/QtCore/*.*.*"][0]
