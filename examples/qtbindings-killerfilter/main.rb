5ea87bda525197c517e01a33ad30bb186ad92a40
John Doe <johndoe@example.com>
2025-10-05 09:36:40 +0800

    docs: Add FilterKiller example

---
+++ examples/qtbindings-killerfilter/main.rb
@@ -0,0 +1,37 @@
+require 'qt6/all'
+
+class KillerFilter < RubyQt6::Bando::QWidget
+  def initialize
+    super
+
+    resize(230, 130)
+
+    pb = QPushButton.new('pushbutton', self)
+    pb.set_geometry(10, 10, 100, 50)
+    pb.install_event_filter(self)
+
+    le = QLineEdit.new('lineedit', self)
+    le.set_geometry(10, 70, 100, 50)
+    le.install_event_filter(self)
+
+    cb = QCheckBox.new('checkbox', self)
+    cb.set_geometry(120, 10, 100, 50)
+    cb.install_event_filter(self)
+
+    rb = QRadioButton.new('radiobutton', self)
+    rb.set_geometry(120, 70, 100, 50)
+    rb.install_event_filter(self)
+  end
+
+  def event_filter(watched, event)
+    return false unless event.type == QEvent::MouseButtonPress
+    return false unless event.button == Qt::RightButton
+    watched.close
+    true
+  end
+end
+
+app = QApplication.new(ARGV)
+widget = KillerFilter.new
+widget.show
+app.exec
