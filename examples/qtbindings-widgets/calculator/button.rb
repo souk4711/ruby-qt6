05a4b2cb4cd2ba632546cbd4661956ade575db44
John Doe <johndoe@example.com>
2025-10-09 07:21:07 +0800

    docs: Update examples

--- examples/qtbindings-widgets/calculator/button.rb
+++ examples/qtbindings-widgets/calculator/button.rb
@@ -11,4 +11,11 @@ class CalcButton < RubyQt6::Bando::QToolButton
     new_palette.set_color(QPalette::Button, palette.color(QPalette::Button))
     set_palette(new_palette)
   end
+
+  def size_hint
+    _size_hint.tap do |size|
+      size.set_height size.height + 20
+      size.set_width size.width > size.height ? size.width : size.height
+    end
+  end
 end
