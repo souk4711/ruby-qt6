913be91998822dc8af39386a442f7ee0b7d57e5b
John Doe <johndoe@example.com>
2025-10-09 13:05:33 +0800

    feat: Update QChar

--- qtcore/lib/qt6/qtcore/qchar.rb
+++ qtcore/lib/qt6/qtcore/qchar.rb
@@ -4,6 +4,33 @@ module RubyQt6
   module QtCore
     # @see https://doc.qt.io/qt-6/qchar.html
     class QChar
+      # @!parse class Category        ; end
+      # @!parse class CombiningClass  ; end
+      # @!parse class Decomposition   ; end
+      # @!parse class Direction       ; end
+      # @!parse class JoiningType     ; end
+      # @!parse class Script          ; end
+      # @!parse class SpecialCharacter; end
+      # @!parse class UnicodeVersion  ; end
+      rubyqt6_declare_enum_under QChar, QChar::Category
+      rubyqt6_declare_enum_under QChar, QChar::CombiningClass
+      rubyqt6_declare_enum_under QChar, QChar::Decomposition
+      rubyqt6_declare_enum_under QChar, QChar::Direction
+      rubyqt6_declare_enum_under QChar, QChar::JoiningType
+      rubyqt6_declare_enum_under QChar, QChar::Script
+      rubyqt6_declare_enum_under QChar, QChar::SpecialCharacter
+      rubyqt6_declare_enum_under QChar, QChar::UnicodeVersion
+
+      # @!visibility private
+      alias_method :_initialize, :initialize
+
+      # @return [QChar]
+      #
+      # @overload initialize(code)
+      #   @param code [Integer]
+      def initialize(*args)
+        _initialize(*args)
+      end
     end
   end
 end
