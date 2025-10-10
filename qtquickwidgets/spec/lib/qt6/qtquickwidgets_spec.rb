0a2a546fc8d3ad60ede06e705a25b01ca8037243
John Doe <johndoe@example.com>
2025-10-10 14:02:48 +0800

    feat: Update qtquickwidget gem

--- qtquickwidgets/spec/lib/qt6/qtquickwidgets_spec.rb
+++ qtquickwidgets/spec/lib/qt6/qtquickwidgets_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe RubyQt6::QtQuickWidgets do
   describe "Bindings" do
     qmod = OpenStruct.new(name: "QtQuickWidgets")
 
-    Dir.glob("ext/**/bando*.cpp").each do |cppfile|
+    Dir.glob("ext/**/bando-q*.cpp").each do |cppfile|
       it cppfile do
         RubyQt6::RSpec.verify_bando_cppfile cppfile, qmod
       end
