c7da5b8c383728988eb42c06969c5459570e194c
John Doe <johndoe@example.com>
2025-10-10 14:02:13 +0800

    feat: Update qtquickcontrols2 gem

--- qtquickcontrols2/spec/lib/qt6/qtquickcontrols2_spec.rb
+++ qtquickcontrols2/spec/lib/qt6/qtquickcontrols2_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe RubyQt6::QtQuickControls2 do
   describe "Bindings" do
     qmod = OpenStruct.new(name: "QtQuickControls2")
 
-    Dir.glob("ext/**/bando*.cpp").each do |cppfile|
+    Dir.glob("ext/**/bando-q*.cpp").each do |cppfile|
       it cppfile do
         RubyQt6::RSpec.verify_bando_cppfile cppfile, qmod
       end
