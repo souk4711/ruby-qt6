b8b16c33fd60411f523f9dc7c8a600be2e5d1025
John Doe <johndoe@example.com>
2025-10-10 20:01:37 +0800

    feat: Update QTooltip

--- qtwidgets/ext/qt6/qtwidgets/qtooltip-rb.cpp
+++ qtwidgets/ext/qt6/qtwidgets/qtooltip-rb.cpp
@@ -10,6 +10,8 @@ void Init_qtooltip(Rice::Module rb_mQt6QtWidgets)
     rb_cQToolTip =
         // RubyQt6::QtWidgets::QToolTip
         define_class_under<QToolTip>(rb_mQt6QtWidgets, "QToolTip")
+            // RubyQt6-Defined Functions
+            .define_singleton_function("show_text", [](const QPoint &pos, const char *text, QWidget *w, const QRect &rect, int msecDisplayTime) -> void { return QToolTip::showText(pos, text, w, rect, msecDisplayTime); }, Arg("pos"), Arg("text"), Arg("w") = static_cast<QWidget *>(nullptr), Arg("rect") = static_cast<const QRect &>(QRect {}), Arg("msec_show_time") = static_cast<int>(-1))
             // Static Public Members
             .define_singleton_function("font", &QToolTip::font)
             .define_singleton_function("hide_text", &QToolTip::hideText)
