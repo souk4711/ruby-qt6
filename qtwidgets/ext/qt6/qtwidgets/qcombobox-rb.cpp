589ed4d535dadfd01de9e1493f16c98e96f95ccc
John Doe <johndoe@example.com>
2025-10-12 17:16:24 +0800

    feat: Update QComboBox

--- qtwidgets/ext/qt6/qtwidgets/qcombobox-rb.cpp
+++ qtwidgets/ext/qt6/qtwidgets/qcombobox-rb.cpp
@@ -15,6 +15,8 @@ void Init_qcombobox(Rice::Module rb_mQt6QtWidgets)
         // RubyQt6::QtWidgets::QComboBox
         define_class_under<QComboBox, QWidget>(rb_mQt6QtWidgets, "QComboBox")
             // RubyQt6-Defined Functions
+            .define_method("add_item", [](QComboBox *self, const QIcon &icon, const char *text, const QVariant &user_data) -> void { return self->addItem(icon, text, user_data); }, Arg("icon"), Arg("text"), Arg("user_data") = static_cast<const QVariant &>(QVariant()))
+            .define_method("add_item", [](QComboBox *self, const char *text, const QVariant &user_data) -> void { return self->addItem(text, user_data); }, Arg("text"), Arg("user_data") = static_cast<const QVariant &>(QVariant()))
             .define_singleton_function("_static_meta_object", []() -> const QMetaObject * { return &QComboBox::staticMetaObject; })
             // Constructor
             .define_constructor(Constructor<QComboBox, QWidget *>(), Arg("parent"))
