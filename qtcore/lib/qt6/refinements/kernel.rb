34533f3b454f9772fe1af64e96444d248ce7c397
John Doe <johndoe@example.com>
2025-10-13 13:36:36 +0800

    feat: Update QFlags

--- qtcore/lib/qt6/refinements/kernel.rb
+++ qtcore/lib/qt6/refinements/kernel.rb
@@ -3,24 +3,17 @@
 # @!visibility private
 module Kernel
   def rubyqt6_declare_enum_under(mod, enum)
-    enum.constants.each do |const|
-      mod.const_set(const, enum.const_get(const))
-    end
-    enum.define_method(:inspect) do
-      RubyQt6::T.q_inspect_enum(self, int: to_i)
-    end
+    enum.constants.each { |const| mod.const_set(const, enum.const_get(const)) }
+    enum.define_method(:inspect) { RubyQt6::T.q_inspect_enum(self, int: to_i) }
   end
 
   def rubyqt6_declare_qflags(flags, enum)
-    enum.define_method(:to_qflags) do
-      flags.new(self)
-    end
-    enum.define_method(:|) do |other|
-      flags.new(self).set_flag(other)
-    end
+    enum.define_method(:to_qflags) { flags.new(self) }
+    enum.define_method(:|) { |other| flags.new(self).set_flag(other) }
 
-    flags.define_method(:inspect) do
-      RubyQt6::T.q_inspect_enum(self, int: to_i)
-    end
+    flags.define_method(:to_qflags) { self }
+    flags.define_method(:zero?) { to_i.zero? }
+    flags.define_method(:nonzero?) { to_i.nonzero? }
+    flags.define_method(:inspect) { RubyQt6::T.q_inspect_enum(self, int: to_i) }
   end
 end
