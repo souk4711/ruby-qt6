92405c27238513e5aba96cc1833831a0f56b5050
John Doe <johndoe@example.com>
2025-10-17 00:24:58 +0800

    docs: Update examples

--- examples/qtbindings-layouts/borderlayout/window.rb
+++ examples/qtbindings-layouts/borderlayout/window.rb
@@ -9,14 +9,14 @@ class Window < RubyQt6::Bando::QWidget
     central_widget = QTextBrowser.new
     central_widget.set_plain_text tr('Central widget')
 
-    layout = BorderLayout.new
-    layout.add_widget(central_widget, BorderLayout::CENTER)
-    layout.add_widget(create_label('North'), BorderLayout::NORTH)
-    layout.add_widget(create_label('West'), BorderLayout::WEST)
-    layout.add_widget(create_label('East 1'), BorderLayout::EAST)
-    layout.add_widget(create_label('East 2'), BorderLayout::EAST)
-    layout.add_widget(create_label('South'), BorderLayout::SOUTH)
-    set_layout(layout)
+    @layout = BorderLayout.new
+    @layout.add_widget(central_widget, BorderLayout::CENTER)
+    @layout.add_widget(create_label('North'), BorderLayout::NORTH)
+    @layout.add_widget(create_label('West'), BorderLayout::WEST)
+    @layout.add_widget(create_label('East 1'), BorderLayout::EAST)
+    @layout.add_widget(create_label('East 2'), BorderLayout::EAST)
+    @layout.add_widget(create_label('South'), BorderLayout::SOUTH)
+    set_layout(@layout)
 
     set_window_title(tr('Border Layout'))
   end
--- examples/qtbindings-layouts/flowlayouts/window.rb
+++ examples/qtbindings-layouts/flowlayouts/window.rb
@@ -6,13 +6,13 @@ class Window < QWidget
   def initialize(parent = nil)
     super
 
-    layout = FlowLayout.new
-    layout.add_widget(QPushButton.new(tr('Short')))
-    layout.add_widget(QPushButton.new(tr('Longer')))
-    layout.add_widget(QPushButton.new(tr('Different text')))
-    layout.add_widget(QPushButton.new(tr('More text')))
-    layout.add_widget(QPushButton.new(tr('Even longer button text')))
-    set_layout(layout)
+    @layout = FlowLayout.new
+    @layout.add_widget(QPushButton.new(tr('Short')))
+    @layout.add_widget(QPushButton.new(tr('Longer')))
+    @layout.add_widget(QPushButton.new(tr('Different text')))
+    @layout.add_widget(QPushButton.new(tr('More text')))
+    @layout.add_widget(QPushButton.new(tr('Even longer button text')))
+    set_layout(@layout)
 
     set_window_title(tr('Flow Layout'))
   end
