953d0e9ff6ca570b5dd5680e953fcc34a87906f8
John Doe <johndoe@example.com>
2025-10-16 23:48:51 +0800

    docs: Add qtbindings-widgets/borderlayout example

---
+++ examples/qtbindings-layouts/borderlayout/borderlayout.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+
+class BorderLayout < RubyQt6::Bando::QLayout
+  WEST   = 0
+  NORTH  = 1
+  SOUTH  = 2
+  EAST   = 3
+  CENTER = 4
+
+  MINIMUM_SIZE = 0
+  SIZE_HINT = 1
+
+  ItemWrapper = Struct.new :item, :position
+
+  def initialize(parent = nil, margin = 0, spacing = -1)
+    super(parent)
+
+    @list = []
+    set_contents_margins(QMargins.new(margin, margin, margin, margin))
+    set_spacing(spacing)
+  end
+
+  def add_item(item)
+    add(item, WEST)
+  end
+
+  def add_widget(widget, position)
+    add(QWidgetItem.new(widget), position)
+  end
+
+  def expanding_directions
+    Qt::Horizontal | Qt::Vertical
+  end
+
+  def has_height_for_width
+    false
+  end
+
+  def count
+    @list.length
+  end
+
+  def item_at(index)
+    wrapper = @list[index]
+    return wrapper.item unless wrapper.nil?
+
+    nil
+  end
+
+  def minimum_size
+    calculate_size(MINIMUM_SIZE)
+  end
+
+  def set_geometry(rect)
+    center = 0
+    east_width = 0
+    west_width = 0
+    north_height = 0
+    south_height = 0
+
+    _set_geometry(rect)
+
+    (0...@list.length).each do |i|
+      wrapper = @list[i]
+      item = wrapper.item
+      position = wrapper.position
+
+      case position
+      when NORTH
+        item.set_geometry QRect.new(rect.x, north_height, rect.width, item.size_hint.height)
+        north_height += item.geometry.height + spacing
+      when SOUTH
+        item.set_geometry QRect.new(item.geometry.x, item.geometry.y, rect.width, item.size_hint.height)
+        south_height += item.geometry.height + spacing
+        item.set_geometry QRect.new(rect.x, rect.y + rect.height - south_height + spacing, item.geometry.width,
+                                    item.geometry.height)
+      when CENTER
+        center = wrapper
+      end
+    end
+
+    center_height = rect.height - north_height - south_height
+
+    (0...@list.length).each do |i|
+      wrapper = @list[i]
+      item = wrapper.item
+      position = wrapper.position
+
+      if position == WEST
+        item.set_geometry QRect.new(rect.x + west_width, north_height, item.size_hint.width, center_height)
+        west_width += item.geometry.width + spacing
+      elsif position == EAST
+        item.set_geometry QRect.new(item.geometry.x, item.geometry.y, item.size_hint.width, center_height)
+        east_width += item.geometry.width + spacing
+        item.set_geometry QRect.new(rect.x + rect.width - east_width + spacing, north_height, item.geometry.width,
+                                    item.geometry.height)
+      end
+    end
+
+    return if center.nil?
+
+    center.item.set_geometry QRect.new(west_width, north_height, rect.width - east_width - west_width, center_height)
+  end
+
+  def size_hint
+    calculate_size(SIZE_HINT)
+  end
+
+  def take_at(index)
+    if index >= 0 && index < @list.length
+      layout_struct = @list.delete_at(index)
+      return layout_struct.item
+    end
+    nil
+  end
+
+  def add(item, position)
+    @list << ItemWrapper.new(item, position)
+  end
+
+  def calculate_size(size_type)
+    total_size = QSize.new
+
+    (0...@list.length).each do |i|
+      wrapper = @list[i]
+      position = wrapper.position
+
+      item_size = size_type == MINIMUM_SIZE ? wrapper.item.minimum_size : wrapper.item.size_hint
+      total_size.set_height(total_size.height + item_size.height) if [NORTH, SOUTH, CENTER].include?(position)
+      total_size.set_width(total_size.width + item_size.width) if [WEST, EAST, CENTER].include?(position)
+    end
+    total_size
+  end
+end
---
+++ examples/qtbindings-layouts/borderlayout/window.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'borderlayout'
+
+class Window < RubyQt6::Bando::QWidget
+  def initialize(parent = nil)
+    super
+
+    central_widget = QTextBrowser.new
+    central_widget.set_plain_text tr('Central widget')
+
+    layout = BorderLayout.new
+    layout.add_widget(central_widget, BorderLayout::CENTER)
+    layout.add_widget(create_label('North'), BorderLayout::NORTH)
+    layout.add_widget(create_label('West'), BorderLayout::WEST)
+    layout.add_widget(create_label('East 1'), BorderLayout::EAST)
+    layout.add_widget(create_label('East 2'), BorderLayout::EAST)
+    layout.add_widget(create_label('South'), BorderLayout::SOUTH)
+    set_layout(layout)
+
+    set_window_title(tr('Border Layout'))
+  end
+
+  def create_label(text)
+    label = QLabel.new(text)
+    label.set_frame_style QFrame::Box.to_i | QFrame::Raised
+    label
+  end
+end
