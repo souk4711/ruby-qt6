27b0f49398ac2c4ede1d6f3086aed6baf952c41f
John Doe <johndoe@example.com>
2025-10-21 22:19:17 +0800

    docs: Update examples

--- examples/pyside6-tutorials/08-qrcfiles/rc_icons.rb
+++ examples/pyside6-tutorials/08-qrcfiles/rc_icons.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-require 'qt6/all'
-
 # $ rcc icons.qrc -binary -o icons.qrc.rcc
 QResource.register_resource(File.join(__dir__, 'icons.qrc.rcc'))
--- examples/pyside6-tutorials/09-translations/rc_linguist.rb
+++ examples/pyside6-tutorials/09-translations/rc_linguist.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'qt6/all'
-
 # $ lrelease example_de.ts
 # $ rcc linguist.qrc -binary -o linguist.qrc.rcc
 QResource.register_resource(File.join(__dir__, 'linguist.qrc.rcc'))
