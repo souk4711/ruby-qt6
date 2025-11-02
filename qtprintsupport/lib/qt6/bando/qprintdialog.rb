253363b713902353071863432c7b34e4f7c73e4d
John Doe <johndoe@example.com>
2025-11-02 23:15:47 +0800

    feat: Remove Bando::QPrintDialog

--- qtprintsupport/lib/qt6/bando/qprintdialog.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RubyQt6
-  module Bando
-    class QPrintDialog < RubyQt6::QtPrintSupport::QPrintDialog
-      Bando.define_bando_qlass Bando::QPrintDialog
-    end
-  end
-end
