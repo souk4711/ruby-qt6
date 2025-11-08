1625b9ca579a13de2d24cc23ca677d7d6efdd4f7
John Doe <johndoe@example.com>
2025-11-09 02:28:49 +0800

    docs: Add qtbindings-itemviews/dirview example

---
+++ examples/qtbindings-itemviews/dirview/main.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'qt6/all'
+
+app = QApplication.new(ARGV)
+
+model = QFileSystemModel.new
+model.set_root_path(QDir.current_path)
+
+tree = QTreeView.new
+tree.set_model(model)
+tree.set_root_index(model.index(QDir.current_path))
+tree.set_window_title(QObject.tr('Dir View'))
+tree.resize(640, 480)
+tree.show
+
+app.exec
