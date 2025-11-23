b7e7cd4c2e5c319efe11bfb5bd0fb6d77d808f7c
John Doe <johndoe@example.com>
2025-11-24 06:55:02 +0800

    feat: Update qtcore

--- qtcore/lib/qt6/refinements/object.rb
+++ qtcore/lib/qt6/refinements/object.rb
@@ -2,11 +2,14 @@
 
 # @!visibility private
 class Object
+  private
+
+  def _gc_incref(object)
+    self.class.__send__(:_gc_refs) << object
+  end
+
   def self._gc_refs
     @_gc_refs ||= []
   end
-
-  def _gc_incref(object)
-    self.class._gc_refs << object
-  end
+  private_class_method :_gc_refs
 end
